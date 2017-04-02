24fed6a7a9af19e64248cd8d7e32cba5ed294fe8
Pedro Faria <pfaf09@gmail.com>
2017-04-02 18:06:31 +0100

    Added node and edge

---
+++ Edge.java
@@ -0,0 +1,34 @@
+
+public abstract class Edge {
+	private static int intid = 0;
+	private int graphid;
+	private Node destino;
+	private int distancia;
+
+	public Edge(Node destino, int distancia){
+		this.destino = destino;
+		this.distancia = distancia;
+		this.graphid = intid;
+		intid++;
+	}
+	
+	public Node getDestino(){
+		return destino;
+	}
+	
+	public void setDestino(Node destino){
+		this.destino = destino;
+	}
+	
+	public int getDistancia(){
+		return distancia;
+	}
+	
+	public void setDistancia(int distancia){
+		this.distancia = distancia;
+	}
+	
+	public int getGraphid() {
+		return this.graphid;
+	}
+}
---
+++ Node.java
@@ -0,0 +1,134 @@
+import java.util.Vector;
+
+public abstract class Node {
+	private static int intid = 0;
+	private int graphid;
+	private int valor;
+	private int tempdist;
+	private Vector<String> temproute;
+	private boolean tempvisited;
+	private String id;
+	private Vector<Edge> arestas;
+	private boolean isGarbage;
+	
+	public Node(String id){
+		this.id = id;
+		Vector<Edge> emptyedge = null;
+		this.arestas = emptyedge;
+		this.valor = -1;
+		this.isGarbage = false;
+		this.tempdist = Integer.MAX_VALUE;
+		Vector<String> empty = null;
+		this.temproute = empty;
+		this.tempvisited = false;
+
+		this.graphid = intid;
+		intid++;
+	}
+	
+	public Node(String id, int valor) {
+		this.id = id;
+		Vector<Edge> emptyedge = null;
+		this.arestas = emptyedge;
+		this.valor = valor;
+		this.isGarbage = true;
+		this.tempdist = Integer.MAX_VALUE;
+		Vector<String> empty = null;
+		this.temproute = empty;
+		this.tempvisited = false;
+
+		this.graphid = intid;
+		intid++;
+	}
+	
+	public Node(String id, Vector<Edge> arestas){
+		this.id = id;
+		this.arestas = arestas;
+		this.valor = -1;
+		this.isGarbage = false;
+		this.tempdist = Integer.MAX_VALUE;
+		Vector<String> empty = null;
+		this.temproute = empty;
+		this.tempvisited = false;
+
+		this.graphid = intid;
+		intid++;
+	}
+	
+	public Node(String id, int valor, Vector<Edge> arestas){
+		this.id = id;
+		this.arestas = arestas;
+		this.valor = valor;
+		this.isGarbage = true;
+		this.tempdist = Integer.MAX_VALUE;
+		Vector<String> empty = null;
+		this.temproute = empty;
+		this.tempvisited = false;
+
+		this.graphid = intid;
+		intid++;
+	}
+	public Vector<Edge> getArestas(){
+		return arestas;
+	}
+	
+	public void setArestas(Vector<Edge> arestas){
+		this.arestas = arestas;
+	}
+	public String getId(){
+		return id;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	
+	public boolean isIsGarbage(){
+		return isGarbage;
+	}
+	
+	public void setIsGarbage(boolean isGarbage){
+		this.isGarbage = isGarbage;
+	}
+	
+	public int getValor(){
+		return valor;
+	}
+	
+	public void setValor(int valor){
+		this.valor = valor;
+	}
+	
+	public int getTempdist(){
+		return tempdist;
+	}
+	
+	public void setTempdist(int tempdist){
+		this.tempdist = tempdist;
+	}
+	
+	
+	public Vector<String> getTemproute(){
+		return temproute;
+	}
+	
+	public void setTemproute(Vector<String> temproute){
+		this.temproute = temproute;
+	}
+	
+	public boolean isTempvisited(){
+		return tempvisited;
+	}
+	
+	public void setTempvisited(boolean tempvisited){
+		this.tempvisited = tempvisited;
+	}
+	
+	public void addEdge(Edge e){
+		this.arestas.add(e);
+	}
+
+	int getGraphid() {
+		return graphid;
+	}
+}
